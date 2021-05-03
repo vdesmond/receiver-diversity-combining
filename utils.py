1cea95148417f259070662a4a3cbdc145354c88a
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-05-03 22:41:28 +0530

    base methods

---
+++ utils.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import numpy as np 
+
+def get_ber(A, B, sample_num):
+    # ! possible error
+    error_count = np.count_nonzero(A==B)
+    # print(error_count)
+    Pe = error_count/sample_num/2
+    return Pe
+
+def equal_gain(g2, r, sample_num, data):
+    r_egc = np.real(np.sum(np.multiply(np.exp( 0-1j * np.angle(g2)), r), axis = 2))
+    # print(r_egc)
+    result = (r_egc > 0).astype(int) * 2 - 1
+    ber = get_ber(result, data, sample_num)
+    return [ber, result]
