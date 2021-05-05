18728be0a30845a8bd5514cda96de6be2765fa02
Vignesh Desmond <vignesh.desmond@gmail.com>
2021-05-05 13:27:31 +0530

    added direct

--- utils.py
+++ utils.py
@@ -1,16 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import numpy as np
-
-def get_bit_error_rate(arr1, arr2, sample_num):
-    error_count = np.count_nonzero(arr1!=arr2)
-    ber = (error_count/sample_num) * 2
-    return ber
+from itertools import chain, combinations
 
 def equal_gain(gain, rec_data, sample_num, data):
     rec_egc = np.sum(np.exp( 0-1j * np.angle(gain)) * rec_data, axis = 2)
     rec_egc_real = np.real(rec_egc)
-    result_egc = (rec_egc_real > 0).astype(int) * 2 - 1
+    result_egc = qpsk_detection(rec_egc_real)
     
     ber_egc = get_bit_error_rate(result_egc, data, sample_num)
     return ber_egc
@@ -18,7 +14,33 @@ def equal_gain(gain, rec_data, sample_num, data):
 def maximal_ratio(gain, rec_data, sample_num, data):
     rec_mrc = np.sum(gain.conjugate() * rec_data, axis=2)
     rec_mrc_real = np.real(rec_mrc)
-    result_mrc = (rec_mrc_real > 0).astype(int) * 2 - 1
+    result_mrc = qpsk_detection(rec_mrc_real)
     
     ber_mrc = get_bit_error_rate(result_mrc, data, sample_num)
     return ber_mrc
+
+def direct(gain_qpsk, rec_data, sample_num, data):
+    # ? channel fading is not needed here, the argument is just a placeholder. 
+    rec_dir = np.sum(rec_data, axis=2)
+    rec_dir_real = np.real(rec_dir)
+    result_dir = qpsk_detection(rec_dir_real)
+
+    ber_dir = get_bit_error_rate(result_dir, data, sample_num)
+    return ber_dir
+
+def qpsk_detection(data):
+    return (data > 0).astype(int) * 2 - 1
+
+def get_bit_error_rate(arr1, arr2, sample_num):
+    error_count = np.count_nonzero(arr1!=arr2)
+    ber = (error_count/sample_num) * 2
+    return ber
+
+def check_modes(mode):
+    sorted_mode = tuple(sorted(mode))
+    all_modes = ("dir","egc","mrc","sel")
+    all_combs = list(chain(*(list(combinations(all_modes, i + 1)) for i in range(len(all_modes)))))
+    if sorted_mode in all_combs:
+        return sorted_mode
+    else:
+        return 0
